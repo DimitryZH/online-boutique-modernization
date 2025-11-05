868055005c0383e0d3eb7aecf757c83669569a3c
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2025-11-05 15:05:51 -0500

    add locust_logs_highload.txt

--- e-commerce-observability-platform/load-tests/locust/analyze_locust_logs.py
+++ e-commerce-observability-platform/load-tests/locust/analyze_locust_logs.py
@@ -2,7 +2,7 @@ import re
 from collections import defaultdict
 
 # Path to the Locust log file to analyze. Change this if your logs are named differently.
-logfile = "default_locust_test_logs.txt"   # locust_logs_highload.txt
+logfile = "locust_logs_highload.txt"
 
 
 # Captures named groups:
